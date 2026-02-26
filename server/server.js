f574300b785a556249260b218f45f957ade11f43
Priyanka Samota <priyankasamota946@gmail.com>
2026-02-26 22:32:36 +0530

    final mail fix

--- server/server.js
+++ server/server.js
@@ -14,11 +14,7 @@ const app = express();
 connectDB();
 
 // 🧩 Middleware
-app.use(cors({
-  origin: ["http://localhost:3000", "https://tent-house-project.vercel.app"],
-  methods: ["GET","POST","PUT","DELETE"],
-  credentials: false,
-}));
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
